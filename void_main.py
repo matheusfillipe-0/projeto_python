4349e840a7f6f234a92a5f62a9ee792d7c5e7ac9
matheusf <matheusf@SPVAL1N-CWYJWP3.pitagoras.apollo.br>
2023-10-08 17:19:08 -0300

    teste permissão de push

--- void_main.py
+++ void_main.py
@@ -1,11 +1,11 @@
-# Criar listas vazias para armazenar informações de login e senha
+ # Criar listas vazias para armazenar informações de login e senha
 from f_login import realizar_login
 from f_registrar import registrar
 from f_printar import mostrar_registro
 from f_verifica_number import verificar_number1
 
 
-
+#teste permissão git
    
 # Menu principal
 
